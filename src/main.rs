901e68867bf917e8b6e78340c201ed3c35956e55
Samuel Lyche <lychesamuel@gmail.com>
2025-08-29 23:17:25 -0700

    subscriptions and telemetry tracing and bunyan

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if we can't read configuration
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres;");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
